481123610039ae4a1afd133c0b430aa5db969100
Adrian Ferrer <adriferrer0799@gmail.com>
2021-11-19 21:43:43 -0500

    Validando errores en vista de egresado

--- views/report/egresado.php
+++ views/report/egresado.php
@@ -21,7 +21,8 @@ $form1 = ActiveForm::begin();
 $egresadoData = app\models\db\EgresadoDe::find()->all();
 $listaEgresado = \yii\helpers\BaseArrayHelper::map($egresadoData, 'id', 'lugar');
 $cursoData = app\models\db\Curso::find()->all();
-$listaCursos = \yii\helpers\BaseArrayHelper::map($cursoData, 'id', 'curso')
+$listaCursos = \yii\helpers\BaseArrayHelper::map($cursoData, 'id', 'curso');
+        $cantEstudiante=0;
 
 
 ?>
@@ -56,23 +57,24 @@ $listaCursos = \yii\helpers\BaseArrayHelper::map($cursoData, 'id', 'curso')
     <div class="col">Nombre y apellidos</div>
 </div>
 <div id="feedback">
-    <?php if ($seleccionEgresado != "")
+    <?php if ($seleccionEgresado != "" && $seleccionCurso != "")
     {
         //echo $seleccionEgresado;
         $estudiante = Estudiante::find()->where(['id_egresado' => $seleccionEgresado, 'id_curso' =>$seleccionCurso])->orderBy("carne")->all();
         $cantEstudiante=Estudiante::find()->where(['id_egresado' => $seleccionEgresado, 'id_curso' =>$seleccionCurso])->count();
-    }
-    else
-    { //echo "Es nulo";
-        $estudiante=Estudiante::find()->orderBy("carne")->all();
-        $cantEstudiante=Estudiante::find()->count();}?>
-    <?php foreach($estudiante as $est) { ?>
-        <div class="col"><?php echo $est->nombre?></div>
+     foreach($estudiante as $est) { ?>
+    <div class="col"><?php echo $est->nombre?></div>
 
 
-        <?php
+    <?php
 
     }  ?><hr>
+    <?php }
+    else if(($seleccionEgresado == "" && $seleccionCurso != "") || ($seleccionEgresado != "" && $seleccionCurso == "") )
+    { //echo "Es nulo";
+       echo '<script>alert("Debe seleccionar ambos campos")</script>';
+        }?>
+
     <div class="col-lg-offset-10 col-lg-11">
         Total de estudiantes :<?php echo $cantEstudiante; ?>
     </div>
